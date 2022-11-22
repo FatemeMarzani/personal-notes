f3ef59e8b7c5d8f88f1be11b42e95144851d32c6
Fateme Marzani <f.marzani.ut@gmail.com>
2022-11-22 16:11:10 +0300

    Getting products info

--- javaScript/shopping-cart/app.js
+++ javaScript/shopping-cart/app.js
@@ -5,7 +5,14 @@ class Product {
         try {
             const result=await fetch('products.json')
             const data=await result.json()
-            return data
+            let products=data.items
+            products=products.map((item)=>{
+                const {title,price}=item.fields
+                const {id}=item.sys
+                const image=item.fields.image.fields.file.url
+                return {title,price,id,image}
+            })
+            return products
         } catch (error) {
             console.log(error)
         }
