b27fc200347317fc541a8160dd290ce5a1794968
Fateme Marzani <f.marzani.ut@gmail.com>
2022-11-25 09:12:19 +0300

    Redirect undefined route to home page

--- javaScript/spa-project/app.js
+++ javaScript/spa-project/app.js
@@ -4,6 +4,7 @@ const router=()=>{
         {path:'/last-videos',view:()=>{console.log("/last-videos")}},
         {path:'/last-posts',view:()=>{console.log("/last-posts")}}
     ]
+
     const matchRoutes=routes.map((item)=>{
         return{
             route : item,
@@ -11,7 +12,16 @@ const router=()=>{
         }
     })
 
-    console.log(matchRoutes)
+    let match=matchRoutes.find((item)=>{
+        return item.isMatch
+    })
+    if(!match){
+        match={route:routes[0], isMatch:true} 
+    }
+
+
+
+    console.log(match.route.view())
 }
 
 
